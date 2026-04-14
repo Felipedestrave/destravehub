0af38816b6b83232a90fab48c5b1cfb86942792a
Felipe Kawakami <destraveseujaponesbackup@gmail.com>
2026-04-14 10:18:31 -0300

    chore: configure Vercel adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   integrations: [react()],
   alias: {
     "@": fileURLToPath(new URL('./src', import.meta.url))
